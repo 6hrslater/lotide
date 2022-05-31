52db6ccd2eeb05bcdb024fd5d1b6c02aedce7197
Darian Chen <darian.chen8639@gmail.com>
2022-05-31 00:54:46 -0700

    this isnt correct yet will need changes later

---
+++ head.js
@@ -0,0 +1,14 @@
+const head = ["Hello", "Lighthouse", "Labs"] 
+    console.log(head[0])
+
+
+
+ // VVVV original assert equal code VVVV   
+ //   if (actual !== unexpected) {
+ //     console.log(`🛑🛑🛑 Assertion Failed: ${head} !== ${notHead}`);
+ //   } else {
+ //     if (actual === unexpected)
+ //       console.log(`✅✅✅ Assertion Passed: ${head} === ${notHead}`);
+ // 
+    
+  
