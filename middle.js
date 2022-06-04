fc1a7cb8748c71d896ba1fbe4f37865a76e98102
Darian Chen <darian.chen8639@gmail.com>
2022-06-03 20:19:31 -0700

    fixed errors

--- middle.js
+++ middle.js
@@ -1,15 +1,14 @@
-const eqArrays = function (array1, array2) {
-    if (array1.length !== array2.length) {
-        return false
+const eqArrays = function(array1, array2) {
+    if (array1.length !== array2.length) 
+      return false;
+    for (let i = array1.length; i--;) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
     }
-        for (let i = 0; i < array1.length; i++) {
-            if (array1[i] !== array2[i]) {
-                return false
-            }
-    
-        }
-        return true
-    };
+    return true;
+  }
+  
 
   const assertArraysEqual = function (array1, array2) {
     let eqvArray = eqArrays(array1, array2)
@@ -20,20 +19,18 @@ const eqArrays = function (array1, array2) {
    }
   }
 
-const middle =  function(array) {
-    let array1 = [];
-    if (array.length < 2) {
-        return array1;
-    } else if ((array.length % 2) !== 0) {
-        let i = Math.floor(array.length / 2);
-        array1.push(array[i]);
-        return array1     
-    } else if ((array.lentgh % 2) === 0) {
-        let k = array.length / 2;
-        let p = k- 1;
-        array1.push(array[p], array[j]);
-        return array1;
+const middle = function (array) {
+    if (array.length <= 2) {
+        return [];
+    } else if (array.length % 2 === 0) {
+        let i = array.length / 2 - 1;
+        let j = array.length / 2;
+        return [array[i], array[j]]
+    } else {
+        return [array[Math.floor(array.length / 2)]];
     }
 };
 
-assertArraysEqual((middle([1,2,3,4,5])), [2,3])
+
+
+
