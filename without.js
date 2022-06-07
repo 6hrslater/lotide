c4cb19c4d42eb620a1085781f5768a845316edec
Darian Chen <darian.chen8639@gmail.com>
2022-06-06 20:55:03 -0700

    Added export function

--- without.js
+++ without.js
@@ -30,10 +30,13 @@ let without = function (source, remover) {
         }
     }
     return array
-}
-console.log(without([1, 2, 3], [1]));
-console.log(without(["1", "2", "3"], [1, 2, "3"]))
+};
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]);
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+module.exports = without;
+
+// console.log(without([1, 2, 3], [1]));
+// console.log(without(["1", "2", "3"], [1, 2, "3"]))
+
+// const words = ["hello", "world", "lighthouse"];
+// without(words, ["lighthouse"]);
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
