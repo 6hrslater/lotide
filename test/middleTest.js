668dbf12522624d80d1065003e9d2f224985948f
Darian Chen <darian.chen8639@gmail.com>
2022-06-06 20:29:36 -0700

    tests files

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,11 @@
 const assert = require('chai').assert;
 const middle = require('../middle');
 
-describe("#Middle", () => {
-    it("returns 2 for [1, 2, 3]", () => {
-      assert.strictEqual(middle([1, 2, 3]), 2);
+describe("#middle", () => {
+    it("returns 2, 3 for [1, 2, 3]", () => {
+      assert.deepEqual(middle([1, 2, 3]), [2]);
     });
-    it("returns '5' for ['5']", () => {
-        assert.strictEqual(middle(['5']), '5'); 
+    it("returns [] for ['5']", () => {
+        assert.deepEqual(middle(['5']), []); 
       });
   })
