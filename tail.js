ce2aae39bb6a13fd23dd08430e52ed30f5f721b2
Darian Chen <darian.chen8639@gmail.com>
2022-06-06 17:32:07 -0700

    Refactored with assertEqual

--- tail.js
+++ tail.js
@@ -1,13 +1,15 @@
-const assertEqual = require('./assertEqual');
+const assertEqual = require('../assertEqual');
   
 const tail = function(array) {
     return array.slice(1)
   }
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+// // Test Case: Check the original array 
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3);
+
+module.exports = tail;
